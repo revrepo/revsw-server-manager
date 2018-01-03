345b5219b1da8258379b334a3dd0e3b2a24bb0db
Victor Gartvich <victor@revsw.com>
2018-01-03 20:31:07 +0000

    Fix for server type

--- code_dir/server_deployment/infradb.py
+++ code_dir/server_deployment/infradb.py
@@ -55,7 +55,7 @@ class InfraDBAPI():
                 "status": 'ONLINE',
                 "location": self.location['id'],
                 "hostingprovider": self.hosting['id'],
-                "type": 1,
+                "type": "BP",
                 "IP": ip,
             }
         server_data.update(server_versions)
