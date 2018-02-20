c4c0639c0d2152258d1247a6b028085b08c7f351
Victor Gartvich <victor@revsw.com>
2018-02-20 01:14:43 +0000

    Move upgrade scripts to a separate dir

--- code_dir/examples_of_upload_and_upgrade_scripts/test-script.sh
+++ code_dir/examples_of_upload_and_upgrade_scripts/test-script.sh
@@ -6,14 +6,13 @@ testCommands=(
      "sudo /usr/lib/nagios/plugins/check_http -I localhost -u http://rum01.revsw.net/service -e 200"
      "sudo /usr/lib/nagios/plugins/check_http -I localhost -u http://www.victor-gartvich.com/ -e 200"
      "sudo /usr/lib/nagios/plugins/check_http -I localhost -u http://www.forgestrategy.com/ -e 200"
-     "sudo /usr/lib/nagios/plugins/check_http -I localhost -u http://cdn.mbeans2.com/images/billboards/Home/main/2015-uppababy-billboard-full.jpg -e 200"
-     "sudo /usr/lib/nagios/plugins/check_http -I localhost -u http://mbeans.com/ -e 200"
+     "sudo /usr/lib/nagios/plugins/check_http -I localhost -u http://cdn.mbeans2.com/images/billboards/Home/main/2015-uppababy-billboard-full.jpg -e 301"
+     "sudo /usr/lib/nagios/plugins/check_http -I localhost -u http://mbeans.com/ -e 301"
      "sudo /usr/lib/nagios/plugins/check_http -I localhost -u http://www.revsw.com/ -e 301"
-     "sudo /usr/lib/nagios/plugins/check_http -I localhost -u http://cdn.turbonomic.com/wp-content/plugins/wp-views/embedded/res/css/wpv-pagination.css -e 200"
-     "sudo /usr/lib/nagios/plugins/check_http -I localhost -u http://m.jisusaiche.com/js/fz-1.3.6-min.js -e 200"
+     "sudo /usr/lib/nagios/plugins/check_http -I localhost -u http://cdn.turbonomic.com/wp-content/plugins/wp-views/embedded/res/css/wpv-pagination.css -e 302"
      "sudo /usr/lib/nagios/plugins/check_http -I localhost -u http://cdn.footankleinstitute.com/Image/Common/Quote.png -e 200"
      "sudo /usr/lib/nagios/plugins/check_http -I localhost -u http://www.revapm.com/ -e 301"
-     "sudo /usr/lib/nagios/plugins/check_http -I localhost -u https://www.revapm.com/ --ssl -e 200"
+     "sudo /usr/lib/nagios/plugins/check_http -I localhost -u https://www.revapm.com/ --ssl -e 301"
      "sudo /usr/lib/nagios/plugins/check_http -I localhost -u http://cdn.footankleinstitute.com/images/quotes/Source/AngiesList.png -e 200"
      "sudo /usr/lib/nagios/plugins/check_http -I localhost -u http://cdn.golflocker.com/images/items/auto/000/01F/0E/inter_i7950.jpg -e 200"
    )
@@ -33,4 +32,3 @@ for (( i = 0; i < ${#testCommands[@]} ; i++ )); do
 done
 
 exit 0
-
